060d8c56c825e24004eba05e538b3b36c83b0f09
sfmasoka <fsmasoka@gmail.com>
2024-09-30 13:48:19 +0200

    Changes on Main labels

--- Interest_Calculator_SF_MASOKA_39069656/Main.Designer.cs
+++ Interest_Calculator_SF_MASOKA_39069656/Main.Designer.cs
@@ -49,6 +49,7 @@ namespace Interest_Calculator_SF_MASOKA_39069656
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.AdditionalPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -228,7 +229,7 @@ namespace Interest_Calculator_SF_MASOKA_39069656
             this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(66, 12);
+            this.label3.Location = new System.Drawing.Point(75, 12);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(160, 22);
             this.label3.TabIndex = 11;
@@ -238,11 +239,11 @@ namespace Interest_Calculator_SF_MASOKA_39069656
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(112, 26);
+            this.label4.Location = new System.Drawing.Point(155, 65);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(258, 17);
+            this.label4.Size = new System.Drawing.Size(170, 17);
             this.label4.TabIndex = 12;
-            this.label4.Text = "SITHEMBISO MASOKA - 39069656";
+            this.label4.Text = "SITHEMBISO MASOKA";
             // 
             // panel2
             // 
@@ -257,9 +258,10 @@ namespace Interest_Calculator_SF_MASOKA_39069656
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.label4);
-            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel3.Location = new System.Drawing.Point(317, 12);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(471, 105);
@@ -268,12 +270,22 @@ namespace Interest_Calculator_SF_MASOKA_39069656
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(180, 63);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(201, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.Size = new System.Drawing.Size(89, 20);
             this.label2.TabIndex = 13;
-            this.label2.Text = "BWIA121 - 2021";
+            this.label2.Text = "Developed ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(234, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "By";
             // 
             // Main
             // 
@@ -325,6 +337,7 @@ namespace Interest_Calculator_SF_MASOKA_39069656
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label2;
     }
 }
